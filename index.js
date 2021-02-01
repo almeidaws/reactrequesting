4f577e2757510aab3db1d31195c7b7ffb8b2b8a2
Gustavo Almeida <almeidaws@outlook.com>
2021-02-01 07:38:19 -0300

    Create useScript

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+export * from "./Scripts";
 export { default as useRequest } from "./useRequest";
 export { default as ResponseError } from "./ResponseError";
 export { default as useFiredRequest } from "./useFiredRequest";
 export { default as useQueryParams } from "./useQueryParams";
 export { default as useQueryStateParam } from "./useQueryStateParam";
 export { default as useUrl } from "./useUrl";
-export {default as useUrlEvent} from "./useUrlEvent"
+export { default as useUrlEvent } from "./useUrlEvent";
