dc64e853a282c96d3c53d3eb992d55d5e06e35d0
Gustavo Almeida <almeidaws@outlook.com>
2021-04-06 13:41:09 -0300

    Export useFiredScript and useScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export * from './Scripts';
 export { default as useRequest } from './useRequest';
 export { default as useFiredRequest } from './useFiredRequest';
 export { default as useUrl } from './useUrl';
